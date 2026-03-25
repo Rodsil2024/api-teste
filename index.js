6926340d30eec57680f1d80f4d6668f43def1cb5
Rodrigo Silva <rsilva.jiul@gmail.com>
2026-03-25 11:59:14 -0300

    fix host render

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.use('/api', testRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
